3d07ab3671bc46d910c6443fdb5644e458ae88c6
root <sajad.karim@gmail.com>
2020-08-20 17:12:05 +0200

    Adding class related changes and some UI related fixes.

---
+++ module/VuFindGEI/src/VuFindGEI/Autocomplete/Solr.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VuFindGEI\Autocomplete;
+
+class Solr extends \VuFind\Autocomplete\Solr
+{
+
+
+}
+
---
+++ module/VuFindGEI/src/VuFindGEI/Autocomplete/Suggester.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VuFindGEI\Autocomplete;
+
+class Suggester extends \VuFind\Autocomplete\Suggester
+{
+
+
+}
+
