4e252dd95e8986cbf5c36ad7989a1eb1b2152cc0
Sajad Karim <sajad.karim@gmail.com>
2020-09-30 13:56:06 +0200

    Enabled existing tour steps and made fixes where necessary. Also made tour similar across all the available pages.

--- local/cache/configs/e15909546de3a4049bb9a39712e8dc41/module-classmap-cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'VuFindTheme\\Module' => '/usr/local/vufind/module/VuFindTheme/Module.php',
-  'VuFindSearch\\Module' => '/usr/local/vufind/module/VuFindSearch/Module.php',
-  'VuFind\\Module' => '/usr/local/vufind/module/VuFind/Module.php',
-  'VuFindAdmin\\Module' => '/usr/local/vufind/module/VuFindAdmin/Module.php',
-  'VuFindApi\\Module' => '/usr/local/vufind/module/VuFindApi/Module.php',
-  'VuFindGEI\\Module' => '/usr/local/vufind/module/VuFindGEI/Module.php',
-);
